09339c15b2209a3e9ec74cf1d3468cb4c1df7da0
Milana <milanin7147@gmail.com>
2022-07-04 20:56:42 +0400

    some changes

--- HW_07/test1.spec.ts
+++ HW_07/test1.spec.ts
@@ -1,18 +1,8 @@
 import {Builder, By, WebDriver, WebElement} from "selenium-webdriver";
+import assert from "assert";
 
-describe("My first suite", () => {
-    // it("My first test", async () => {
-    //     const driver: WebDriver = await new Builder().forBrowser("chrome").build();
-    //     await driver.manage().window().maximize();
-    //     await driver.get("https://rabota.by");
-    //     const input: WebElement = await driver.findElement(By.css('[data-qa="search-input"]'));
-    //     await input.sendKeys("TypeScript");
-    //     const search: WebElement = await driver.findElement(By.css('button[data-qa="search-button"]'));
-    //     await search.click();
-    //     //await driver.manage().setTimeouts({implicit: 5000})
-    //
-    //     await driver.quit();
-    // });
+describe("Check login page", () => {
+
     it("Test 2", async () => {
         const driver: WebDriver = await new Builder().forBrowser("chrome").build();
         await driver.manage().window().maximize();
@@ -20,6 +10,7 @@ describe("My first suite", () => {
         const input: WebElement = await driver.findElement(By.xpath("//div[text()=\"Вход\"]"));
         await input.click();
         await driver.findElement(By.xpath("//button[@class=\"auth-button auth-button_primary auth-button_middle auth-form__button auth-form__button_width_full\"]")).getText()
+        await driver.manage().setTimeouts({implicit: 5000})
         await driver.quit();
     });
 });
