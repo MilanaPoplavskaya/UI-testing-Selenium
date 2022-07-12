3aaca9b180d76c5652f15b0acc2e1a8338e8c71b
Milana <milanin7147@gmail.com>
2022-07-12 10:59:56 +0400

    5 tests for onliner.by

---
+++ HW_07/locators.ts
@@ -0,0 +1,25 @@
+const loginButton: string = '//div[text()="Вход"]';
+const loginLink: string = 'a[class="auth-button auth-button_subsidiary auth-button_huge auth-form__button auth-form__button_fb"]';
+const cartButton: string = 'a[class="auth-bar__item auth-bar__item--cart"]';
+const text: string = 'div[class="cart-form__title cart-form__title_big-alter cart-form__title_condensed-additional"]';
+const input: string = "fast-search__input";
+const frame: string = '//iframe[@class="modal-iframe"]';
+const searchImage: string = 'div[class="search__content-wrapper"]';
+const linkToTheNews: string = 'a[href="https://people.onliner.by/opinions/2022/07/07/ukraina-134-j-den"]';
+const checkImage: string = "news-header__image";
+const searchLink: string = '//span[text()="Мангалы, грили, барбекю"]';
+const searchButton: string = '//span[text()="Объявления"]';
+export {
+    loginButton,
+    loginLink,
+    cartButton,
+    text,
+    input,
+    frame,
+    searchImage,
+    linkToTheNews,
+    checkImage,
+    searchLink,
+    searchButton
+
+};
--- HW_07/test1.spec.ts
+++ HW_07/test1.spec.ts
@@ -1,16 +1,78 @@
-import {Builder, By, WebDriver, WebElement} from "selenium-webdriver";
+import {Builder, By, until, WebDriver, WebElement} from "selenium-webdriver";
 import assert from "assert";
+import * as Assert from "assert";
+import {
+    loginLink,
+    loginButton,
+    cartButton,
+    text,
+    input,
+    frame,
+    searchImage,
+    linkToTheNews,
+    checkImage,
+    searchLink,
+    searchButton
+} from "./locators";
 
-describe("Check login page", () => {
 
-    it("Test 2", async () => {
-        const driver: WebDriver = await new Builder().forBrowser("chrome").build();
+const driver: WebDriver = new Builder().forBrowser("chrome").build();
+const URL: string = "https://www.onliner.by/";
+
+
+describe("onliner.by testing", () => {
+
+    it("Check login page", async () => {
         await driver.manage().window().maximize();
-        await driver.get("https://www.onliner.by/");
-        const input: WebElement = await driver.findElement(By.xpath("//div[text()=\"Вход\"]"));
-        await input.click();
-        await driver.findElement(By.xpath("//button[@class=\"auth-button auth-button_primary auth-button_middle auth-form__button auth-form__button_width_full\"]")).getText()
-        await driver.manage().setTimeouts({implicit: 5000})
+        await driver.get(URL);
+        const element: WebElement = await driver.findElement(By.xpath(loginButton));
+        await element.click();
+        await driver.wait(until.elementLocated(By.css(loginLink)));
+        const link: WebElement = await driver.findElement(By.css(loginLink));
+        assert(link.isDisplayed(), "Error: login page is not opened");
+
+        await driver.quit();
+    });
+    it("Check shopping cart", async () => {
+        await driver.manage().window().maximize();
+        await driver.get(URL);
+        const cart: WebElement = await driver.findElement(By.css(cartButton));
+        await cart.click();
+        await driver.wait(until.elementLocated(By.css(text)));
+        const checkText: WebElement = await driver.findElement(By.css(text));
+        Assert.equal(await checkText.getText(), "Корзина");
+        await driver.quit();
+
+    });
+    it("Check input field", async () => {
+        await driver.manage().window().maximize();
+        await driver.get(URL);
+        const inputField: WebElement = await driver.findElement(By.className(input));
+        await inputField.sendKeys("Lenovo");
+        await driver.switchTo().frame(driver.findElement(By.xpath(frame)));
+        const image: WebElement = await driver.findElement(By.css(searchImage));
+        assert(image.isDisplayed(), "Error: image is not displayed");
+
+        await driver.quit();
+
+
+    });
+    it("Check news link", async () => {
+        await driver.manage().window().maximize();
+        await driver.get(URL);
+        const newsLink: WebElement = await driver.findElement(By.css(linkToTheNews));
+        await newsLink.click();
+        const image: WebElement = await driver.findElement(By.className(checkImage));
+        assert(image.isDisplayed(), "Error:image is not displayed");
+    });
+    it("Check is the button enabled", async () => {
+        await driver.manage().window().maximize();
+        await driver.get(URL);
+        const newsLink: WebElement = await driver.findElement(By.xpath(searchLink));
+        await newsLink.click();
+        await driver.wait(until.elementLocated(By.xpath(searchButton)));
+        const checkButton: WebElement = await driver.findElement(By.xpath(searchButton));
+        assert(checkButton.isEnabled(), "Error: button is not enabled");
         await driver.quit();
     });
 });
